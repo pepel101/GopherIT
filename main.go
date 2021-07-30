b0b424541858125c33e68d452b71d96a7b47ce3d
pepel101 <marcsgilman@gmail.com>
2021-07-30 21:11:19 +0300

    adding room from file functionality, gonna check

--- main.go
+++ main.go
@@ -6,10 +6,14 @@ import (
 
 func main() {
 	ch := make(chan SessionEvent)
-
 	world := NewWorld()
 	world.Init()
 
+	rooms, err1 := roomsInit()
+	if err1 != nil {
+		log.Fatal(err1)
+	}
+	world.rooms = append(world.rooms, rooms...)
 	sessionHandler := NewSessionHandler(world, ch)
 	go sessionHandler.Start()
 
